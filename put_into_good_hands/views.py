437fb44b00fb0d1c62fdfe44a6c922c80739be5c
PrzemekJackowski <przemek.jackowski1304@gmail.com>
2022-07-11 22:57:04 +0200

    Create first views and paths

--- put_into_good_hands/views.py
+++ put_into_good_hands/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views import View
 
-# Create your views here.
+
+class LandingPageView(View):
+    def get(self, request):
+        return render(request, "index.html", {})
+
+
+class AddDonationView(View):
+    def get(self, request):
+        return render(request, "form.html",  {})
+
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, "login.html", {})
+
+
+class RegisterView(View):
+    def get(self, request):
+        return render(request, "register.html", {})
