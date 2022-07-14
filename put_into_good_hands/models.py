2c0daa2cc1ded29e51245506816fcb87ebaba8b0
PrzemekJackowski <przemek.jackowski1304@gmail.com>
2022-07-14 21:21:57 +0200

    create models

--- put_into_good_hands/models.py
+++ put_into_good_hands/models.py
@@ -1,3 +1,33 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+TYPES = (
+    (1, "Fundacja"),
+    (2, "Organizacja pozarządowa"),
+    (3, "Zbiórka lokalna"),
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.BigIntegerField()
+    city = models.CharField(max_length=64)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
