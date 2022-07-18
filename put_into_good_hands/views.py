ee8b4b90cba9ba98803168f503996c7042810e8e
PrzemekJackowski <przemek.jackowski1304@gmail.com>
2022-07-18 19:59:55 +0200

    Create a navigation on index.html page

--- put_into_good_hands/views.py
+++ put_into_good_hands/views.py
@@ -4,10 +4,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.views import View
 
+from put_into_good_hands.models import Donation
+
 
 class LandingPageView(View):
     def get(self, request):
-        return render(request, "index.html", {})
+        gifts = Donation.objects.all()
+        quantity = 0
+        institutions = []
+        for gift in gifts:
+            quantity += gift.quantity
+            if gift.institution not in institutions:
+                institutions.append(gift.institution.name)
+        count_institutions = len(institutions)
+        return render(request, "index.html", {"quantity": quantity, "count_institutions": count_institutions})
 
 
 class AddDonationView(View):
